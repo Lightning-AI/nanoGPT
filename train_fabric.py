0da0a8706865ea1f41499e71bcbcd83b31d51679
awaelchli <aedu.waelchli@gmail.com>
2023-01-18 20:18:58 +0100

    update commands

--- train_fabric.py
+++ train_fabric.py
@@ -3,16 +3,16 @@ This training script can be run both on a single gpu in debug mode,
 and also in a larger training run with distributed data parallel (ddp).
 
 To run on a single GPU, example:
-$ python train.py --batch_size=32 --compile=False
+$ lightning run model --accelerator=cuda --precision=bf16 train_fabric.py
 
 To run with DDP on 4 gpus on 1 node, example:
-$ torchrun --standalone --nproc_per_node=4 train.py
+$ lightning run model --accelerator=cuda --precision=bf16 --devices=4 train_fabric.py
 
 To run with DDP on 4 gpus across 2 nodes, example:
 - Run on the first (master) node with example IP 123.456.123.456:
-$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr=123.456.123.456 --master_port=1234 train.py
+$ lightning run model --accelerator=cuda --precision=bf16 --devices=8 --num_nodes=2 --node_rank=0 --main_address=123.456.123.456 --main_port=1234 train_fabric.py
 - Run on the worker node:
-$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=123.456.123.456 --master_port=1234 train.py
+$ lightning run model --accelerator=cuda --precision=bf16 --devices=8 --num_nodes=2 --node_rank=1 --main_address=123.456.123.456 --main_port=1234 train_fabric.py
 (If your cluster does not have Infiniband interconnect prepend NCCL_IB_DISABLE=1)
 """
 
