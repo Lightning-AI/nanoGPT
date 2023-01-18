f2913701c95b9866d0efa91e41a3b45deb31319d
awaelchli <aedu.waelchli@gmail.com>
2023-01-18 20:02:49 +0100

    launch

--- train_fabric.py
+++ train_fabric.py
@@ -72,8 +72,7 @@ config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
 
 # Try also strategy="ddp" or strategy="deepspeed" with devices > 1
-fabric = Fabric(accelerator="cuda", devices=4, strategy="ddp", precision="bf16")
-fabric.launch()
+fabric = Fabric(accelerator="cuda", precision="bf16")
 
 master_process = fabric.global_rank == 0
 
