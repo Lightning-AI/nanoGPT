64a10aa2949a1766420d6a8f3251a61bcc264804
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-28 19:18:13 +0100

    profile

--- train_fabric.py
+++ train_fabric.py
@@ -58,7 +58,7 @@ n_embd = 768
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 # adamw optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 5 # total number of training iterations
+max_iters = 50 # total number of training iterations
 weight_decay = 1e-2
 beta1 = 0.9
 beta2 = 0.95
@@ -120,37 +120,8 @@ if init_from == 'scratch':
     print("Initializing a new model from scratch")
     gptconf = GPTConfig(**model_args)
     model = GPT(gptconf)
-elif init_from == 'resume':
-    print(f"Resuming training from {out_dir}")
-    # resume training from a checkpoint.
-    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
-    checkpoint = torch.load(ckpt_path, map_location=device)
-    checkpoint_model_args = checkpoint['model_args']
-    for k, v in model_args.items():
-        assert checkpoint_model_args[k] == v, "for now"
-        # TODO: think through how passed in params should interact with checkpoint params
-    gptconf = GPTConfig(**model_args)
-    model = GPT(gptconf)
-    state_dict = checkpoint['model']
-    # fix the keys of the state dictionary :(
-    # honestly no idea how checkpoints sometimes get this prefix, have to debug more
-    unwanted_prefix = '_orig_mod.'
-    for k,v in list(state_dict.items()):
-        if k.startswith(unwanted_prefix):
-            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
-    model.load_state_dict(state_dict)
-    iter_num = checkpoint['iter_num']
-    best_val_loss = checkpoint['best_val_loss']
-elif init_from.startswith('gpt2'):
-    print(f"Initializing from OpenAI GPT-2 weights: {init_from}")
-    # initialize from OpenAI GPT-2 weights
-    override_args = dict(dropout=dropout)
-    model = GPT.from_pretrained(init_from, override_args)
-    # read off and override the GPT sizing model args from the model config
-    model_args['n_layer'] = model.config.n_layer
-    model_args['n_head'] = model.config.n_head
-    model_args['n_embd'] = model.config.n_embd
-# crop down the model block size if desired
+
+
 if block_size < model.config.block_size:
     model.crop_block_size(block_size)
 
@@ -160,12 +131,6 @@ optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta
 if init_from == 'resume':
     optimizer.load_state_dict(checkpoint['optimizer'])
 
-# compile the model
-if compile:
-    print("compiling the model... (takes a ~minute)")
-    unoptimized_model = model
-    model = torch.compile(model) # requires PyTorch 2.0
-
 
 # setup according to the precision, accelerator and strategy passed in
 # to the Fabric constructor, that is:
@@ -174,53 +139,11 @@ if compile:
 # 3. wrap the model according to the chosen strategy
 model, optimizer = fabric.setup(model, optimizer)
 
-
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
-# learning rate decay scheduler (cosine with warmup)
-def get_lr(iter):
-    # 1) linear warmup for warmup_iters steps
-    if iter < warmup_iters:
-        return learning_rate * iter / warmup_iters
-    # 2) if iter > lr_decay_iters, return min learning rate
-    if iter > lr_decay_iters:
-        return min_lr
-    # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (iter - warmup_iters) / (lr_decay_iters - warmup_iters)
-    assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
-    return min_lr + coeff * (learning_rate - min_lr)
-
-# logging
-if wandb_log and master_process:
-    import wandb
-    wandb.init(project=wandb_project, name=wandb_run_name, config=config)
-
-# training loop
-t0 = time.time()
-with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+timings = []
+with profile(activities=[ProfilerActivity.CUDA]) as prof:
 
     while True:
-
-        # determine the learning rate for this iteration
-        if decay_lr:
-            lr = get_lr(iter_num)
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
-        else:
-            lr = learning_rate
+        t0 = time.perf_counter()
 
         # forward backward update, with optional gradient accumulation to simulate larger batch size
         optimizer.zero_grad(set_to_none=True)
@@ -235,18 +158,12 @@ with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
         with record_function("optimizer_step"):
             optimizer.step()
 
-
-        # timing and logging
-        t1 = time.time()
-        dt = t1 - t0
-        t0 = t1
-        if iter_num % log_interval == 0 and master_process:
-            lossf = loss.item() # loss as float. TODO note CPU-GPU sync! profile, make sure not too slow
-            print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms")
-        iter_num += 1
+        t1 = time.perf_counter()
+        timings.append(t1 - t0)
 
         # termination conditions
         if iter_num > max_iters:
             break
 
+fabric.print("iter time", torch.mean(torch.tensor(timings)).item())
 fabric.print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=100))
